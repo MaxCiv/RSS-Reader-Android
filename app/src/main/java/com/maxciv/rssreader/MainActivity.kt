8ac929ae297473c02402d7b1c789f20cf4d42b53
Max Oleynik <max2896@ya.ru>
2020-03-06 22:13:53 +0300

    Base project configuration

--- app/src/main/java/com/maxciv/rssreader/MainActivity.kt
+++ app/src/main/java/com/maxciv/rssreader/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.maxciv.rssreader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.view_pager_fragment).build()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
